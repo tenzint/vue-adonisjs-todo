0fc73325415981a86d9fd00fe36022e888ef58ea
Tenzin Thabkhae <tenzin.thabkhae@mail.utoronto.ca>
2019-10-23 17:34:06 -0400

    --- Vue --- initial setup

---
+++ client/src/store/index.js
@@ -0,0 +1,19 @@
+import createPersistedState from 'vuex-persistedstate';
+import Vue from 'vue';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  },
+  plugins: [
+    createPersistedState(),
+  ],
+});
